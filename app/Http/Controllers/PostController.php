792c7da3c060f5f3cb46d223f63213c68f8d2869
Lester Hurtado <lester@hashtagdigital.com>
2017-10-18 11:29:26 +0000

    Add replies.

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use App\Http\Requests\StorePostRequest;
+use App\Topic;
+use App\Transformers\PostTransformer;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function store(StorePostRequest $request, Topic $topic)
+    {
+        $post = new Post;
+        $post->body = $request->body;
+        $post->user()->associate($request->user());
+
+        $topic->posts()->save($post);
+
+        return fractal()
+            ->item($post)
+            ->parseIncludes(['user'])
+            ->transformWith(new PostTransformer())
+            ->toArray();
+    }
+}
